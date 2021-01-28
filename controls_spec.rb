7a7926a1d89aeba411a8b8b5a07d0b02addbde0b
Josh Larsen <jlarsen@gmail.com>
2021-01-28 18:18:12 -0500

    aws-117

--- controls_spec.rb
+++ controls_spec.rb
@@ -606,10 +606,29 @@ RSpec.describe "[#{control_id}] #{titles[control_id]}" do
 end
 
 control_id = 'darkbit-aws-117'
+opts = { control_pack: control_pack, control_id: control_id, "#{control_id}": true }
 RSpec.describe "[#{control_id}] #{titles[control_id]}" do
-  describe 'Placeholder', control_pack: control_pack, control_id: control_id, "#{control_id}": true do
-    it 'should not have a placeholder configuration' do
-      expect(true).to eq(true)
+  q = %(
+    MATCH (b:AWS_S3_BUCKET)
+      RETURN b.name AS name,
+             b.is_public AS is_public
+    )
+  buckets = graphdb.query(q).mapped_results
+
+  q = %(
+    MATCH (t:AWS_CLOUDTRAIL_TRAIL)
+    RETURN t.name AS name,
+           t.s3_bucket_name AS s3_bucket_name
+  )
+  trails = graphdb.query(q).mapped_results
+
+  trails.each do |trail|
+    trail_bucket = buckets.find { |b| b.name == "arn:aws:s3:::#{trail.s3_bucket_name}" }
+
+    describe trail.name, opts do
+      it 'should have S3 object level logging to CloudTrail enabled' do
+        expect(trail_bucket&.is_public).to eq('false')
+      end
     end
   end
 end
